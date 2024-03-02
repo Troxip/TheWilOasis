541a2e232e2d167968c2297f3d5f74953630484f
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-02 22:01:30 +0100

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-  background-color: orangered;
-  padding: 20px;
-`;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">The Wild Oasis</Heading>
-
-          <div>
-            <Heading as="h2">Check in and out</Heading>
-            <Button onClick={() => console.log("Check in")}>Check in</Button>
-            <Button onClick={() => console.log("Check out")}>Check out</Button>
-          </div>
-        </Row>
-
-        <Row type="vertical">
-          <Heading as="h3">Form</Heading>
-          <form>
-            <Input type="number" placeholder="Number of guests" />
-            <Input type="number" placeholder="Number of guests" />
-          </form>
-        </Row>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -1,4 +1,31 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { Button } from "./Button";
 
-const Row = styled.div``;
+const Row = styled.div`
+  display: flex;
+
+  ${(props) =>
+    props.type === "horizontal" &&
+    css`
+      justify-content: space-between;
+      align-items: center;
+    `}
+
+  ${(props) =>
+    props.type === "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
+`;
+
+Row.defaultProps = {
+  type: "vertical",
+};
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
+
 export default Row;
