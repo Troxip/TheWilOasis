44991a255e904d41f6e0898101fe99304f348d37
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-10 20:51:59 +0100

    Carts

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import Stats from "./Stats";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
+import { useCabins } from "./../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,30 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const {
+    confirmedStays,
+    stays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today&apos;s activity</div>
+      <div>Chart stay durations</div>
+      <SalesChart />
+    </StyledDashboardLayout>
+  );
+}
---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,59 @@
+import { formatCurrency } from "../../utils/helpers";
+import Stat from "./Stat";
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+
+export default function Stats({
+  bookings,
+  confirmedStays,
+  numDays,
+  cabinCount,
+}) {
+  // 1
+  const numBookings = bookings.length;
+
+  // 2
+  const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  // 3
+  const checkins = confirmedStays.length;
+
+  // 4
+  const occupation =
+    confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (numDays * cabinCount);
+  // num checked in nights / all available nights
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        value={numBookings}
+        icon={<HiOutlineBriefcase />}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        value={formatCurrency(sales)}
+        icon={<HiOutlineBanknotes />}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        value={checkins}
+        icon={<HiOutlineCalendarDays />}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        value={Math.round(occupation * 100) + "%"}
+        icon={<HiOutlineChartBar />}
+      />
+    </>
+  );
+}
